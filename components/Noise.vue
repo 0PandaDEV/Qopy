7345483ba19cc3d01dec50431ee0d5f821f113b1
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2025-03-14 23:59:38 +0100

    refactor: update Noise component styles and replace noise image with a new format; remove unused SVG icons

--- components/Noise.vue
+++ components/Noise.vue
@@ -6,16 +6,18 @@
 .noise {
   position: absolute;
   overflow: hidden;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
+  top: 1px;
+  left: 1px;
+  width: calc(100vw - 2px);
+  height: calc(100vh - 2px);
   pointer-events: none;
   user-select: none;
-  z-index: -1;
-  background-image: url('/noise.png');
+  background-image: url("/noise.webp");
   background-repeat: repeat;
   image-rendering: pixelated;
   overflow: hidden;
+  mix-blend-mode: multiply;
+  opacity: .5;
+  border-radius: 12px;
 }
 </style>
