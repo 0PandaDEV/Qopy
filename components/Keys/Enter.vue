843a1ea8b79d0f887c56b69300ca712dfe1cc70e
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2025-03-16 21:32:20 +0100

    feat: add new key icon components for Cmd, Enter, Key, and Shift to enhance keyboard representation

---
+++ components/Keys/Enter.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    width="24px"
+    height="20px"
+    viewBox="0 0 24 20"
+    version="1.1"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg">
+    <g id="Enter" fill-opacity="1">
+      <path
+        d="M-659 -2016L-659 -2016L-659 -1996L-683 -1996L-683 -2016L-659 -2016Z"
+        id="Enter"
+        fill="none"
+        stroke="none" />
+      <path
+        d="M12 0L17.6 0C19.8402 0 20.9603 0 21.816 0.435974Q22.3804 0.723593 22.8284 1.17157Q23.2764 1.61955 23.564 2.18404C24 3.03969 24 4.15979 24 6.4L24 13.6C24 15.8402 24 16.9603 23.564 17.816Q23.2764 18.3804 22.8284 18.8284Q22.3804 19.2764 21.816 19.564C20.9603 20 19.8402 20 17.6 20L6.4 20C4.15979 20 3.03969 20 2.18404 19.564Q1.61955 19.2764 1.17157 18.8284Q0.723594 18.3804 0.435974 17.816C0 16.9603 0 15.8402 0 13.6L0 6.4C0 4.15979 0 3.03969 0.435974 2.18404Q0.723594 1.61955 1.17157 1.17157Q1.61955 0.723594 2.18404 0.435974C3.03969 0 4.15979 0 6.4 0L12 0Z"
+        id="Rectangle"
+        fill="#FFFFFF"
+        fill-opacity="0.050980393"
+        stroke="none" />
+      <path
+        d="M16.0597 5.48914L16.0597 10.5L7.5 10.5"
+        id="Vector"
+        fill="none"
+        fill-rule="evenodd"
+        stroke="#E5DFD5"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round" />
+      <path
+        d="M9.5 8.5L9.5 12.5035L7 10.5L9.5 8.5Z"
+        id="Vector"
+        fill="#E5DFD5"
+        fill-rule="evenodd"
+        stroke="#E5DFD5"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round" />
+    </g>
+  </svg>
+</template>
---
+++ components/Keys/Key.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="key-container"
+    :style="{
+      backgroundColor: 'var(--border)',
+      padding: '0 7px',
+      height: '20px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '4px',
+      minWidth: '22px'
+    }"
+  >
+    <span
+      :style="{
+        color: '#E5E0D5',
+        fontSize: '12px'
+      }"
+    >
+      {{ input }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  input: string;
+}>();
+</script>
